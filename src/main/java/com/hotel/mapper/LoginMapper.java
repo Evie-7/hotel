72946c4cff28a618d5f43681d9bf1f725cdd55af
Evie123 <779019524@qq.com>
2018-09-25 21:58:29 +0800

    Revert "Initial commit"
    This reverts commit 7f38e3aba2dd7b76d5420dc50c863ba3165a4c8b.

--- src/main/java/com/hotel/mapper/LoginMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.mapper;
-
-import com.hotel.pojo.Admin;
-
-public interface LoginMapper {
-//	根据用户名查询登录信息
-	public Admin loginByName(Admin admin);
-
-}
--- src/main/java/com/hotel/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.hotel.service;
-
-import com.hotel.pojo.Admin;
-
-public interface LoginService {
-//	根据用户名查询登录信息
-	Admin loginByName(Admin admin);
-}
